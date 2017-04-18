2ba73dc837b78ce7c874e4bfd6015100b733ec11
mferreira96 <pount@sapo.pt>
2017-04-18 18:04:48 +0100

    small update on Problem attributes

--- calendarizacao de exames/src/logic/Problem.java
+++ calendarizacao de exames/src/logic/Problem.java
@@ -5,14 +5,19 @@ import java.util.ArrayList;
 public class Problem {
 
 	private ArrayList<Exame> exames;
+	private int numberOfDays;
 	
-	
-	public Problem(ArrayList<Exame> exames){
+	public Problem(ArrayList<Exame> exames,int numberOfDays){
 		
 		this.exames = exames;
+		this.numberOfDays = numberOfDays;
 		
 	}
 	
+	
+	public int getNumberOfDays() {
+		return numberOfDays;
+	}
 	
 	public ArrayList<Exame> getExames() {
 		return exames;
